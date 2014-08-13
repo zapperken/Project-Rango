1e2c602fb6f2038ae4636b3f86bedc70847b7b82
Kendall Capangpangan <zeronitnek@gmail.com>
2014-08-14 00:48:17 +0800

    rango files and project settings updated, added templates

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,7 +1,12 @@
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Rango says hello world! <a href="/about">About</a>')
+    context = RequestContext(request)
+    context_dict = {'boldmessage': "I am bold font from the context"}
+    return render_to_response('rango/index.html', context_dict, context)
+    #return HttpResponse('Rango says hello world! <a href="/rango/about/">About</a>')
 
 def about(request):
-    return HttpResponse('Rango Says: Here is the about page. <a href="/">Home</a>')
+    return HttpResponse('Rango Says: Here is the about page. <a href="/rango/">Home</a>')
