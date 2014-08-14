e018e23797c14dfe937c9fbd9e256ff0bf58e952
Kendall Capangpangan <zeronitnek@gmail.com>
2014-08-14 22:31:38 +0800

    templates, project files updated

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,6 +1,7 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
+import random
 
 def index(request):
     context = RequestContext(request)
@@ -9,4 +10,8 @@ def index(request):
     #return HttpResponse('Rango says hello world! <a href="/rango/about/">About</a>')
 
 def about(request):
-    return HttpResponse('Rango Says: Here is the about page. <a href="/rango/">Home</a>')
+    context = RequestContext(request)
+    what = ['happy','clever','shy']
+    context_dict = {'what': random.choice(what)}
+    return render_to_response('rango/about.html', context_dict, context)
+    #return HttpResponse('Rango Says: Here is the about page. <a href="/rango/">Home</a>')
