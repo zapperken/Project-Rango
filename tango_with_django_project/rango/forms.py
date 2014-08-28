569516a7157f8310151a8fb396893942c703be34
Kendall Capangpangan <zeronitnek@gmail.com>
2014-08-28 16:26:51 +0800

    Updated forms.py and register template

--- tango_with_django_project/rango/forms.py
+++ tango_with_django_project/rango/forms.py
@@ -39,16 +39,22 @@ class PageForm(forms.ModelForm):
         return cleaned_data
         
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
+    username = forms.CharField(help_text="Please enter a username.")
+    email = forms.CharField(help_text="Please enter your email.")
+    password = forms.CharField(widget=forms.PasswordInput(),
+        help_text="Please enter a password.")
     
     class Meta:
         model = User
-        fields = ('username', 'email', 'password')
+        fields = ['username', 'email', 'password']
         
 class UserProfileForm(forms.ModelForm):
+    website = forms.URLField(help_text="Please enter your website.", required=False)
+    picture = forms.ImageField(help_text="Select a profile image to upload.", required=False)
+        
     class Meta:
         model = UserProfile
-        fields = ('website', 'picture')
+        fields = ['website', 'picture']
 
     def clean(self):
         cleaned_data = self.cleaned_data
